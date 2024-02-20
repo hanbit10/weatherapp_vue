a601b07804cee4ffc5722f855af9cde94b06796f
hanbit10 <chanbit10@gmail.com>
2024-02-20 15:07:58 +0100

    app setup and tailwind setup

---
+++ frontend/src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div></div>
+</template>
+
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+
+<style lang="scss" scoped></style>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
